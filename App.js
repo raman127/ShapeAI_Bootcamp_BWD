2d2d69696a1c01f6107c3fbb57d27626452bd5fa
Raman Kumar <37361157+raman127@users.noreply.github.com>
2021-06-05 13:10:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="header">ShapeAI</h1>
+      <div className="first">
+        <h1>Javascript and React.js</h1>
+        <p className="p1">
+          This was an amazing bootcamp taken by Shaurya Sinha.We covered
+          everything from scratch including javascript,React.js and HTML.
+        </p>
+      </div>
+      <div className="second">
+        <h1>Javascript and React.js</h1>
+        <p className="p1">
+          This was an amazing bootcamp taken by Shaurya Sinha.We covered
+          everything from scratch including javascript,React.js and HTML.
+        </p>
+      </div>
+      <div className="first">
+        <h1>Javascript and React.js</h1>
+        <p className="p1">
+          This was an amazing bootcamp taken by Shaurya Sinha.We covered
+          everything from scratch including javascript,React.js and HTML.
+        </p>
+      </div>
+      <div className="second">
+        <h1>Javascript and React.js</h1>
+        <p className="p1">
+          This was an amazing bootcamp taken by Shaurya Sinha.We covered
+          everything from scratch including javascript,React.js and HTML.
+        </p>
+      </div>
+      <div className="first">
+        <h1>Javascript and React.js</h1>
+        <p className="p1">
+          This was an amazing bootcamp taken by Shaurya Sinha.We covered
+          everything from scratch including javascript,React.js and HTML.
+        </p>
+      </div>
+    </div>
+  );
+}
